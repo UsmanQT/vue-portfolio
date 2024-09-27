ca5a1d87185641a326ab1ddf196b8fc77907bb73
usmanqt <qureshiu@mail.gvsu.edu>
2024-09-27 16:57:12 -0400

    created a basic header for the portfolio

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="10">
+                <p>{Icon}</p>
+            </v-col>
+            <v-col cols="2">
+                <v-btn rounded="lg">
+                    Say Hello
+                </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <HelloWorld />
+  <Header />
 </template>
 
 <script setup>

2106483b14e1a3a270988581df6af4ed10101102
usmanqt <qureshiu@mail.gvsu.edu>
2025-02-18 16:19:30 -0500

    changed styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,42 +1,19 @@
 <template>
-    <v-container>
-      <v-row align="center" justify="start">
-        <!-- First Column: Positioned to the extreme left -->
-        <v-col cols="auto">
-          <v-img class="round-image" src="@/assets/Usman.jpg" alt="Icon" height="150" width="150"/>
-        </v-col>
-  
-        <!-- Spacer Column: This takes the remaining space -->
-        <v-col class="d-flex justify-center">
-          <v-row justify="center">
-            <router-link to="/" class="mx-2">
-              <v-btn rounded="lg">
-                Home
-              </v-btn>
-            </router-link>
-            <router-link to="/demo" class="mx-2">
-              <v-btn rounded="lg">
-                Project Demo
-              </v-btn>
-            </router-link>
-            <router-link to="/chat" class="mx-2">
-              <v-btn rounded="lg">
-                Say Hello
-              </v-btn>
-            </router-link>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-container>
+    <v-row align="center" justify="start">
+      <v-col class="d-flex justify-center">
+        <v-row justify="center">
+          <router-link to="/" class="mx-2">
+            <v-btn rounded="lg"> Home </v-btn>
+          </router-link>
+          <router-link to="/demo" class="mx-2">
+            <v-btn rounded="lg"> Project Demo </v-btn>
+          </router-link>
+          <router-link to="/chat" class="mx-2">
+            <v-btn rounded="lg"> Say Hello </v-btn>
+          </router-link>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
-  
-  
-
-<style scoped>
-.round-image {
-  width: 150px; /* Adjust the width as needed */
-  height: 150px; /* Adjust the height as needed */
-  object-fit: cover; /* Ensures the image is clipped properly without stretching */
-  border-radius: 50%; /* Clips the image to a circle */
-}
-</style>
--- src/components/MyInfo.vue
+++ src/components/MyInfo.vue
@@ -1,52 +1,117 @@
 <template>
-    <v-container>
-      <v-col class="mx-auto text-left" :style=" { color: $vuetify.theme.themes.light.primary, textDecoration: 'none' }">
+  <v-container class="mt-10" style="border: 1px solid black; border-radius: 20px">
+    <v-row align="center" justify="center">
+      <v-col cols="auto mx-auto mr-10">
+        <v-img
+          class="round-image"
+          src="@/assets/Usman.jpg"
+          alt="Icon"
+          height="150"
+          width="150"
+        />
+      </v-col>
+      <v-col
+        class="mx-auto text-left"
+        :style="{ color: $vuetify.theme.themes.light.primary, textDecoration: 'none' }"
+      >
         <h1>{{ userInfo.fullName }}</h1>
-        <p>Master's In Computer Science @ GVSU | SDE @ Applied Computing Institute</p>
-        
-        <v-row class="mt-5 mx-auto">
-          <!-- FontAwesome Icon -->
-          <p>{{ userInfo.location }}</p>
+        <p class="mt-2">
+          Master's In Computer Science @ GVSU | SDE @ Applied Computing Institute
+        </p>
+
+        <v-row class="mt-5 mx-auto" align="center">
+          <v-row class="mx-auto" align="center">
+            <font-awesome-icon :icon="['fa', 'map-pin']" class="pr-2" color="black" />
+            <p>{{ userInfo.location }}</p>
+          </v-row>
+          <v-btn
+            style="width: 200px; height: 50px"
+            :href="userInfo.resumeLink"
+            target="_blank"
+            color="primary"
+            class="ml-4 mr-10"
+            >Download Resume</v-btn
+          >
         </v-row>
-  
+
         <v-row class="mx-auto" align="center">
-            <font-awesome-icon :icon="['fas', 'envelope']" class="pr-2" color="black"/>
-          <a :href="`mailto:${userInfo.email}`" :style=" { color: $vuetify.theme.themes.light.primary, textDecoration: 'none' }">{{ userInfo.email }}</a>
+          <font-awesome-icon :icon="['fas', 'envelope']" class="pr-2" color="black" />
+          <a
+            :href="`mailto:${userInfo.email}`"
+            :style="{
+              color: $vuetify.theme.themes.light.primary,
+              textDecoration: 'none',
+            }"
+            >{{ userInfo.email }}</a
+          >
         </v-row>
-  
-        <v-row class="mx-auto" align="center">
-            <font-awesome-icon :icon="['fab', 'linkedin']" class="pr-2" color="black"/>
-          <a :href="userInfo.linkedInLink" :style=" { color: $vuetify.theme.themes.light.primary, textDecoration: 'none'}">in/usmanq</a>
+
+        <v-row class="mx-auto" align="center" justify="space-around">
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="pr-2" color="black" />
+          <v-row class="mx-auto" align="center">
+            <a
+              :href="userInfo.linkedInLink"
+              :style="{
+                color: $vuetify.theme.themes.light.primary,
+                textDecoration: 'none',
+              }"
+              >in/usmanq</a
+            >
+          </v-row>
+          <v-btn
+            style="width: 200px; height: 50px"
+            :href="userInfo.resumeLink"
+            target="_blank"
+            color="primary"
+            class="ml-4 mr-10"
+            >Book a Call</v-btn
+          >
         </v-row>
-  
-        <v-row class="mx-auto" align="center">
-            <font-awesome-icon :icon="['fab', 'github']" class="pr-2" color="black"/>
-          <a :href="userInfo.githubLink" :style=" { color: $vuetify.theme.themes.light.primary, textDecoration: 'none' }">UsmanQT</a>
+
+        <v-row class="mx-auto mb-5" align="center">
+          <font-awesome-icon :icon="['fab', 'github']" class="pr-2" color="black" />
+          <a
+            :href="userInfo.githubLink"
+            :style="{
+              color: $vuetify.theme.themes.light.primary,
+              textDecoration: 'none',
+            }"
+            >UsmanQT</a
+          >
         </v-row>
       </v-col>
-    </v-container>
+    </v-row>
+  </v-container>
 </template>
-  
-  <script>
-  import { userBasicInfo } from '@/constants';
-  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-  import { library } from '@fortawesome/fontawesome-svg-core';
-  import { faFontAwesome } from '@fortawesome/free-brands-svg-icons';
-  import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-  import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
+<script>
+import { userBasicInfo } from "@/constants";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFontAwesome } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faMapPin } from "@fortawesome/free-solid-svg-icons";
 
-  library.add(faFontAwesome, faGithub, faLinkedin, faEnvelope);
-  
-  export default {
-    components: {
-      FontAwesomeIcon // Registering the FontAwesomeIcon component
-    },
-    data() {
-      return {
-        userInfo: userBasicInfo
-      };
-    },
-  };
-  </script>
-  
+library.add(faFontAwesome, faGithub, faLinkedin, faEnvelope, faMapPin);
+
+export default {
+  components: {
+    FontAwesomeIcon, // Registering the FontAwesomeIcon component
+  },
+  data() {
+    return {
+      userInfo: userBasicInfo,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.round-image {
+  width: 150px; /* Adjust the width as needed */
+  height: 150px; /* Adjust the height as needed */
+  object-fit: cover; /* Ensures the image is clipped properly without stretching */
+  border-radius: 50%; /* Clips the image to a circle */
+}
+</style>
